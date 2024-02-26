2e38ec04d2cd4d55e87b68d192afae0e18b543e4
azimkg <azeldenbaev@gmail.com>
2024-02-26 22:48:22 +0600

    upgrades

--- src/components/section/Section.jsx
+++ src/components/section/Section.jsx
@@ -22,7 +22,7 @@ const Section = () => {
     return (
         <div className=" w-full h-[80vh] sm:h-[70vh]">
         <Header  />
-        <div className="w-full bg-[#FEEEDE] flex justify-center 4xl:items-center adaptiv_section overflow-hidde h-[80vh] lg:h-[50vh] xl:h-[60vh] 2xl:h-[70vh] 4xl:h-[70vh] 3xl:h-[81vh]  5xl:h-[90vh] pt-[10px] pb-[3vw] top-[80px]">
+        <div className="w-full bg-[#FEEEDE] flex justify-center 4xl:items-center adaptiv_section overflow-hidde h-[80vh] lg:h-[45vh] xl:h-[55vh] 2xl:h-[60vh] 4xl:h-[65vh] 3xl:h-[75vh]  5xl:h-[90vh] pt-[10px] pb-[3vw] top-[80px]">
             <div className="relative containers flex items-center flex-col gap-10">
                 <img style={{maxWidth:"70%", maxHeight:'70%'}} src={lili} className="relative z-50  mr-[30px]" alt="Lili png" />
                 <Button onClick={openModal} className={'w-[370px] sm:w-[590px] h-[74px] md:h-[90px] xl:h-[83px] 2xl:h-[103px] sm:text-[36px] md:text-[66px]'}>
