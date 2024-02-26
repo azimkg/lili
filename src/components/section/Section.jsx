99a8d47c7c8a7692d4f11da124ef0f2d1e2d8e41
azimkg <azeldenbaev@gmail.com>
2024-02-26 22:21:52 +0600

    upgrades

--- src/components/section/Section.jsx
+++ src/components/section/Section.jsx
@@ -20,9 +20,9 @@ const Section = () => {
         dispatch(resetModalState())
     };
     return (
-        <div className="relative w-full h-[100vh]">
-        <Header className={'absolute top-0 left-0 z-20'} />
-        <div className="w-full bg-[#FEEEDE] flex justify-center 4xl:items-center adaptiv_section overflow-hidden h-[90vh] 5xl:h-[92vh] absolute pt-16 sm:pt-20 md:pt-14 lg:pt-10 4xl:pt-0 pb-[3vw] top-[80px]">
+        <div className=" w-full h-[80vh] sm:h-[70vh]">
+        <Header  />
+        <div className="w-full bg-[#FEEEDE] flex justify-center 4xl:items-center adaptiv_section overflow-hidden h-full 5xl:h-[90vh] pt-16 sm:pt-20 md:pt-14 lg:pt-10 4xl:pt-0 pb-[3vw] top-[80px]">
             <div className="relative containers flex items-center flex-col gap-10">
                 <img src={lili} className="relative z-50 w-[33vh] 4xl:w-[50vh]  mr-[30px]" alt="Lili png" />
                 <Button onClick={openModal} className={'w-[370px] sm:w-[590px] h-[74px] md:h-[90px] xl:h-[83px] 2xl:h-[103px] sm:text-[36px] md:text-[66px]'}>
@@ -33,7 +33,7 @@ const Section = () => {
                 </Modal>
             </div>
         </div>
-        <Footer className={'absolute bottom-0 left-0 z-20'}/>
+        <Footer />
         </div>
     );
 };
